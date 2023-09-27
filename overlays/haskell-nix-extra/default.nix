b178e28726a03e5894a3462851db502e444db117
zeme <lorenzo.calegari@iohk.io>
2023-09-27 10:46:00 +0200

    Typo

--- overlays/haskell-nix-extra/default.nix
+++ overlays/haskell-nix-extra/default.nix
@@ -65,6 +65,7 @@ in final: prev: with final; with lib; {
           rm $out/bin/*${stdenv.hostPlatform.extensions.executable}
           cp --no-preserve=timestamps --recursive ${drv}/bin/*${stdenv.hostPlatform.extensions.executable} $out/bin/
           echo "********************************************************************************"
+          echo "The extension is: ${stdenv.hostPlatform.extensions.executable}"
           ls -lah $out/bin/
           chmod -R +w $out/bin/*${stdenv.hostPlatform.extensions.executable}
           ${pkgsBuildBuild.haskellBuildUtils}/bin/set-git-rev "${gitrev}" $out/bin/*${stdenv.hostPlatform.extensions.executable}
